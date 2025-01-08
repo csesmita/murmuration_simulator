59e3d74d2bf54f439d3a5d4a66ab99f67e9454e9
Smita Vijaya Kumar <sv440@cl.cam.ac.uk>
2025-01-08 15:14:57 +0000

    Script used for simulation results with other schedulers

--- simulation/results/scalability/tmp4.sh
+++ simulation/results/scalability/tmp4.sh
@@ -1,35 +1,35 @@
 #Sparrow
-pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 5000 no 0 2 no Hawk 0 0 && mv finished_file s.11.10X 
-pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 10000 no 0 2 no Hawk 0 0 && mv finished_file s.12.10X
-pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 15000 no 0 2 no Hawk 0 0 && mv finished_file s.13.10X 
-pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 20000 no 0 2 no Hawk 0 0 && mv finished_file s.14.10X
+pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 5000 no 0 2 no Hawk 0 0 no && mv finished_file s.11.10X 
+pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 10000 no 0 2 no Hawk 0 0 no && mv finished_file s.12.10X
+pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 15000 no 0 2 no Hawk 0 0 no && mv finished_file s.13.10X 
+pypy3 eagle_simulation.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 20000 no 0 2 no Hawk 0 0 no && mv finished_file s.14.10X
 #SparrowPT
-pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 5000 no 0 2 no SparrowPT 0 0 && mv finished_file spt.11.10X
-pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 10000 no 0 2 no SparrowPT 0 0 && mv finished_file spt.12.10X
-pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 15000 no 0 2 no SparrowPT 0 0 && mv finished_file spt.13.10X
-pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 20000 no 0 2 no SparrowPT 0 0 && mv finished_file spt.14.10X
+pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 5000 no 0 2 no SparrowPT 0 0 no && mv finished_file spt.11.10X
+pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 10000 no 0 2 no SparrowPT 0 0 no && mv finished_file spt.12.10X
+pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 15000 no 0 2 no SparrowPT 0 0 no && mv finished_file spt.13.10X
+pypy3 eagle_simulation.py  YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 0 0 20000 no 0 2 no SparrowPT 0 0 no && mv finished_file spt.14.10X
 #Murmuration
-pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 5000 no 0 2 no Murmuration 1 0 && mv finished_file m.11.10X
-pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 10000 no 0 2 no Murmuration 1 0 && mv finished_file m.12.10X 
-pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 15000 no 0 2 no Murmuration 1 0 && mv finished_file m.13.10X 
-pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 20000 no 0 2 no Murmuration 1 0 && mv finished_file m.14.10X
+pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 5000 no 0 2 no Murmuration 1 0 no && mv finished_file m.11.10X
+pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 10000 no 0 2 no Murmuration 1 0 no && mv finished_file m.12.10X 
+pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 15000 no 0 2 no Murmuration 1 0 no && mv finished_file m.13.10X 
+pypy3 eagle_simulation.py YH.tr no no -1 -1 100 100 1 2 5 MEAN 0 0 ATC 0 0 20000 no 0 2 no Murmuration 1 0 no && mv finished_file m.14.10X
 #Hawk
-pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 no 0 2 no Hawk 0 0 && mv finished_file h.11.10X 
-pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 no 0 2 no Hawk 0 0 && mv finished_file h.12.10X 
-pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 no 0 2 no Hawk 0 0 && mv finished_file h.13.10X 
-pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 no 0 2 no Hawk 0 0 && mv finished_file h.14.10X
+pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 no 0 2 no Hawk 0 0 no && mv finished_file h.11.10X 
+pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 no 0 2 no Hawk 0 0 no && mv finished_file h.12.10X 
+pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 no 0 2 no Hawk 0 0 no && mv finished_file h.13.10X 
+pypy3 eagle_simulation.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 no 0 2 no Hawk 0 0 no && mv finished_file h.14.10X
 #Eagle
-pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 yes 0 20 yes Eagle 0 0 && mv finished_file e.11.10X 
-pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 yes 0 20 yes Eagle 0 0 && mv finished_file e.12.10X 
-pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 yes 0 20 yes Eagle 0 0 && mv finished_file e.13.10X 
-pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 yes 0 20 yes Eagle 0 0 && mv finished_file e.14.10X
+pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 yes 0 20 yes Eagle 0 0 no && mv finished_file e.11.10X 
+pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 yes 0 20 yes Eagle 0 0 no && mv finished_file e.12.10X 
+pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 yes 0 20 yes Eagle 0 0 no && mv finished_file e.13.10X 
+pypy3 eagle_simulation.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 yes 0 20 yes Eagle 0 0 no && mv finished_file e.14.10X
 #Yaq-C
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 no 0 2 no CLWL 0 0 && mv finished_file yaqc.11.10X
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 no 0 2 no CLWL 0 0 && mv finished_file yaqc.12.10X
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 no 0 2 no CLWL 0 0 && mv finished_file yaqc.13.10X
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 no 0 2 no CLWL 0 0 && mv finished_file yaqc.14.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 no 0 2 no CLWL 0 0 no && mv finished_file yaqc.11.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 no 0 2 no CLWL 0 0 no && mv finished_file yaqc.12.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 no 0 2 no CLWL 0 0 no && mv finished_file yaqc.13.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 no 0 2 no CLWL 0 0 no && mv finished_file yaqc.14.10X
 #Yaq-D
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 yes 8 2 no DLWL 0 0 && mv finished_file yaqd.11.10X
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 yes 8 2 no DLWL 0 0 && mv finished_file yaqd.12.10X
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 yes 8 2 no DLWL 0 0 && mv finished_file yaqd.13.10X
-pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 yes 8 2 no DLWL 0 0 && mv finished_file yaqd.14.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 5000 yes 8 2 no DLWL 0 0 no && mv finished_file yaqd.11.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 10000 yes 8 2 no DLWL 0 0 no && mv finished_file yaqd.12.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 15000 yes 8 2 no DLWL 0 0 no && mv finished_file yaqd.13.10X
+pypy3 eagle_simulation.py YH.tr no yes -1 -1 100 98 1 2 5 MEAN 0 0 ATC 10000 10 20000 yes 8 2 no DLWL 0 0 no && mv finished_file yaqd.14.10X
